e8ab440b81c2eff6e22f80afeacff9d3cf72bb27
Austin Meier <austin.meier@hotmail.com>
2022-02-01 10:49:26 -0700

    test pulling note card data from JsonKepper

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,17 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
 
-export default function Home() {
+interface ExploreData {
+  img: string;
+  location: string;
+  distance: string;
+}
+
+interface CardProps {
+  exploreData: Array<ExploreData>;
+}
+
+export default function Home({ exploreData }: CardProps) {
   return (
     <div className="">
       <Head>
@@ -11,8 +21,31 @@ export default function Home() {
       </Head>
 
       <Header />
-
       <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          {/* Pull some data from the server - API endpoints */}
+          {exploreData?.map((item) => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const exploreData: CardProps = await fetch('https://links.papareact.com/pyp').
+  then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    }
+  }
 }
