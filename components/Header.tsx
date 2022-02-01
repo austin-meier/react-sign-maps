3596ead9be7a26308d66dbd520c7a867046b6eda
Austin Meier <austin.meier@hotmail.com>
2022-02-01 09:13:36 -0700

    Start user menu on navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image'
 import Search from './Search';
+import { GlobeAltIcon,
+    MenuIcon,
+    UserCircleIcon,
+    UsersIcon,    
+} from '@heroicons/react/solid';
 
 function Header() {
   return (
@@ -18,8 +23,14 @@ function Header() {
         <Search />
 
         {/* Right */}
-        <div>
+        <div className='flex items-center space-x-4 justify-end text-gray-500'>
+            <p className='hidden md:inline-flex cursor-pointer'>Become a host</p>
+            <GlobeAltIcon className='h-6 cursor-pointer'/>
 
+            <div className='flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer' >
+                <MenuIcon className='h-6'/>
+                <UserCircleIcon className='h-6' />
+            </div>
         </div>
     </header>
   );
