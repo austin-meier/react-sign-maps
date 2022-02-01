c9cfc56e5478177d0ae1955e670f976e0e22e723
Austin Meier <austin.meier@hotmail.com>
2022-02-01 10:06:53 -0700

    Main page banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import Image from "next/image";
 
 function Banner() {
-  return <div></div>;
+    return (
+        <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+            <Image 
+                src="https://links.papareact.com/0fm"
+                layout="fill"
+                objectFit="cover" 
+            />
+            <div className="absolute top-1/2 w-full text-center">
+                <p className="text-sm sm:text-lg">Not sure where to go? Prefect.</p>
+
+                <button className="my-3 px-10 py-4 shadow-md bg-red-400 text-white font-bold rounded-full hover:shadow-xl active:scale-90 transition duration-150">I'm Flexible</button>
+            </div>
+        </div>
+    );
 }
 
 export default Banner;
